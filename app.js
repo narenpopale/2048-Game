025cec9f09894dfd6e243b6abec1798814853dec
Naren Popale <narenpopale@gmail.com>
2024-09-29 00:09:37 +0530

    Arrows Logic Done

--- app.js
+++ app.js
@@ -22,6 +22,35 @@ const putInitialNumber = () => {
     board[x][y] = 2;
 }
 
+const countEmptyTiles = () => {
+    let cnt = 0;
+
+    for(let arr of board) {
+        for(let num of arr) {
+            if(num == -1) cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+const putNewNumber = () => {
+    let count = countEmptyTiles();
+    let randNum = generateRandNum(count) + 1;
+
+    for(let i = 0;i < 4; i++) {
+        for(let j = 0;j < 4; j++) {
+            if(board[i][j] == -1) {
+                randNum--;
+            }
+            if(randNum == 0) {
+                board[i][j] = 2;
+                randNum--;
+            }
+        }
+    }
+}
+
 newGame.addEventListener("click", () => {
     putInitialNumber();
     putInitialNumber();
@@ -29,7 +58,7 @@ newGame.addEventListener("click", () => {
 })
 
 
-const updateBoard = (key) => {
+const updateBoardLeft = () => {
 
     for(let r = 0;r < 4; r++) {
         let prev = 0;
@@ -55,10 +84,104 @@ const updateBoard = (key) => {
 }
 
 
-document.addEventListener("keydown", (event) => {
-    console.log(event.key);
-    if(event.key == "ArrowLeft") {
-        updateBoard();
-        console.log(board);
+const updateBoardRight = () => {
+
+    for(let r = 0;r < 4; r++) {
+        let prev = 3;
+        for(let c = 2;c >= 0; c--) {
+            if(board[r][c] == -1) continue;
+            if(board[r][prev] == board[r][c]) {
+                board[r][prev] *= 2;
+                board[r][c] = -1;
+                prev--;
+            }
+            else if(board[r][prev] == -1) {
+                board[r][prev] = board[r][c];
+                board[r][c] = -1;
+            }
+            else {
+                prev--;
+                board[r][prev] = board[r][c];
+                if(prev != c) board[r][c] = -1;
+            }
+        }
     }
+
+}
+
+
+const updateBoardUp = () => {
+
+    for(let c = 0;c < 4; c++) {
+        let prev = 0;
+        for(let r = 1;r < 4; r++) {
+            if(board[r][c] == -1) continue;
+            if(board[prev][c] == board[r][c]) {
+                board[prev][c] *= 2;
+                board[r][c] = -1;
+                prev++;
+            }
+            else if(board[prev][c] == -1) {
+                board[prev][c] = board[r][c];
+                board[r][c] = -1;
+            }
+            else {
+                prev++;
+                board[prev][c] = board[r][c];
+                if(prev != r) board[r][c] = -1;
+            }
+        }
+    }
+
+}
+
+
+const updateBoardDown = () => {
+
+    for(let c = 0;c < 4; c++) {
+        let prev = 3;
+        for(let r = 2;r >= 0; r--) {
+            if(board[r][c] == -1) continue;
+            if(board[prev][c] == board[r][c]) {
+                board[prev][c] *= 2;
+                board[r][c] = -1;
+                prev--;
+            }
+            else if(board[prev][c] == -1) {
+                board[prev][c] = board[r][c];
+                board[r][c] = -1;
+            }
+            else {
+                prev--;
+                board[prev][c] = board[r][c];
+                if(prev != r) board[r][c] = -1;
+            }
+        }
+    }
+
+}
+
+
+const updateBoard = (key) => {
+
+    if(key === "ArrowLeft") {
+        updateBoardLeft();
+    }
+    if(key === "ArrowRight") {
+        updateBoardRight();
+    }
+    if(key === "ArrowUp") {
+        updateBoardUp();
+    }
+    if(key === "ArrowDown") {
+        updateBoardDown();
+    }
+    console.log(board);
+
+}
+
+
+document.addEventListener("keydown", (event) => {
+    updateBoard(event.key);
+    putNewNumber();
 })
